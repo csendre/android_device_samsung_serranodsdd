07579ece699bbe1b2043b8d9671ecc193114e72b
csendre <csendre@gmx.net>
2016-04-12 07:57:42 -0400

    Added files via upload

---
+++ pac.mk
@@ -0,0 +1,13 @@
+# Inherit some common PAC stuff.
+$(call inherit-product, vendor/pac/config/common_full_phone.mk)
+
+$(call inherit-product, device/samsung/serranodsdd/full_serranodsdd.mk)
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=serranodsxx TARGET_DEVICE=serranods BUILD_FINGERPRINT="samsung/serranodsxx/serranods:4.4.2/KOT49H/I9192XXUCNG2:user/release-keys" PRIVATE_BUILD_DESC="serranodsxx-user 4.4.2 KOT49H I9192XXUCNG2 release-keys"
+
+PRODUCT_DEVICE := serranodsdd
+PRODUCT_NAME := pac_serranodsdd
